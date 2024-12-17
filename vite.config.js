a8e8a0aa59efa39e74a4cee32850596120e3f940
1conorsteward <1conorsteward@gmail.com>
2024-12-17 14:46:44 -0600

    Updated for github pages.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "@rollup/plugin-commonjs";
+import commonjs from "vite-plugin-commonjs";
 
 export default defineConfig({
   plugins: [react(), commonjs()],
+  base: "/crypto-follower/",
+  optimizeDeps: {
+    include: ["react-chartjs-2"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
